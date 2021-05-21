51cc31e0a0902c8a3970c2f641d0734ca611b0f8
bang387 <52222856+bang387@users.noreply.github.com>
2021-05-22 03:08:53 +0700

    Create loop.sh

---
+++ loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+s=1
+e=10000
+n=1
+for ((i = 0; i<500; i++))
+	do	
+		sed -n "$s,$e"p tel08.txt > "$n".csv;
+		((s=e))
+		((e=e+10000))
+		((n+=1))
+		#echof s: $s
+		#echo e: $e
+		#sed -n 1,10p 0611109104 | cat -n
+	done
